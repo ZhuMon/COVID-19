8af548fb62fa0c9625d1dc59eaab55fa1dcd20db
YuXiang <richardlin0212@gmail.com>
2020-05-07 11:27:37 +0800

    Format C/C++ file with clang-format

--- mac/keylogger.h
+++ mac/keylogger.h
@@ -1,16 +1,16 @@
 #ifndef __KEYLOGGER_H__
 #define __KEYLOGGER_H__
 
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
 #include <ApplicationServices/ApplicationServices.h>
 #include <Carbon/Carbon.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 
 FILE *logfile = NULL;
 const char *logfileLocation = "/var/log/keystroke.log";
 
-CGEventRef CGEventCallback(CGEventTapProxy, CGEventType, CGEventRef, void*);
+CGEventRef CGEventCallback(CGEventTapProxy, CGEventType, CGEventRef, void *);
 const char *convertKeyCode(int);
 
 #endif
