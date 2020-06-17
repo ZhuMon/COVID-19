b397d05ec6613ad51da8cb0c4c5bfb7290bbec1b
YX <richardlin0212@gmail.com>
2020-06-18 01:26:02 +0800

    [TODO] Parse data with json

--- windows/py_server.py
+++ windows/py_server.py
@@ -1,28 +1,102 @@
 from bottle import route, run, request, abort, static_file
+import json
+import re
+import threading
 
-d = []
+all_data = []
+d_count = 0
+win_val_dict = {}
+h_or_d = "" # 0 for header, 1 for data
 @route("/put_data", method="POST")
 def put_data():
+    global all_data
+    global h_or_d
+    global d_count
+    
     for k,v in request.POST.allitems():
-        print(k, v)
 
-    d.append(request.forms.get("mmmm"))
+        for i in v.split("\n\n"):
+            if i != '':
+                # print(i)
+                all_data += [i]
+                # print(all_data)
+                if i.find("{\"window\"") != -1:
+                    h_or_d += "0"
+                else:
+                    h_or_d += "1"
+
+    
+    #d.append(request.forms.get("mmmm"))
+    d_count = len(all_data)
     print("put data")
     return 'OK'
+   
+
+def organize_data():
+    global h_or_d
+    global win_val_dict
+    global d_count
+
+    while d_count == 0:
+        None
     
+    local_d_count = 0
+    window_name = ""
+    data_time = 0
+    while True:
+        if local_d_count < d_count:
+            v = all_data[local_d_count]
+            if v == '':
+                continue
+
+            # header
+            print(v)
+            if h_or_d[local_d_count] == "0":
+                j = json.loads(v)
+                window_name = v["window"]
+                data_time = v["time"]
+                
+            # data
+            else:
+                if window_name not in win_val_dict.keys():
+                    win_val_dict[window_name] = [{"time": data_time, "data": v}]
+                else: 
+                    if data_time > win_val_dict[window_name][-1]["time"] + 300:
+                        win_val_dict[window_name] += {"time": data_time, "data": v}
+                    else:
+                        win_val_dict[window_name][-1]["data"] += v
+   
+
+
+            
+            local_d_count += 1
+
+
+
+def transfer_data(content):
+    # find [#B]
+    b_start = content.find("[#B]")
+    output = str(content)
+    while b_start != -1:
+        output = output[0:b_start-1] + output[b_start:]
+        output = output.replace("[#B]", "", 1)
+        b_start = output.find("[#B]")
+    return output
+
+
 @route("/show", method="GET")
 def show_data():
     # TODO: only administrator can view
     output_content = ""
-    for content in d:
-        output_content = f"{output_content}{content}<br>"
-        print(content)
 
-    return output_content
+    return str(win_val_dict)
     
 @route("/get_data", method="GET")
 def get_data():
     return "get_data"
 
 if __name__ == "__main__":
-    run(host="127.0.0.1", port=8081, debug=True)
+    #run(host="127.0.0.1", port=8081, debug=True)
+    t = threading.Thread(target=run, kwargs=dict(host='localhost', port=8081))
+    t.start()
+    organize_data()
